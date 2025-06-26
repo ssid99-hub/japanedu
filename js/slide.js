d9b9e0625b3902127a61e430101dd01aa766828e
ssid99-hub <saydimuhamad777@gmail.com>
2025-06-26 15:37:50 +0700

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,50 @@
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    // slides.length = 3
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    // chieu tang next
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    // chieu pre giam
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // an het
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block"; // show cai duoc chon
+    dots[slideIndex - 1].className += " active";
+}
+
+function showSlidesAuto() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlidesAuto, 5000); // Change image every 2 seconds
+}
